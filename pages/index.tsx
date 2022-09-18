c9055866045c5c5a22c31c607e277bbb58857bd3
bbOracul <gilyazovRislan@gmail.com>
2022-09-18 15:59:26 +0500

    first page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,32 +1,10 @@
 import type { NextPage } from "next";
-import Head from "next/head";
-import styles from "../styles/Home.module.css";
-import Field, { FieldProps } from "../src/client/components/Field";
-import * as gameAPILib from "../src/sever/gameAPIServerLib";
-import { Coords } from "../src/types";
+import { useRouter } from "next/router";
 
-export async function getServerSideProps(context: any) {
-  const data = await gameAPILib.createGame("test");
-  return {
-    props: data.player0.field, // will be passed to the page component as props
-  };
-}
-
-const Home: NextPage<FieldProps> = (props: FieldProps) => {
-  const FieldProps = props;
-  const handleCellClick = (coords: Coords) => {
-    console.log(coords);
-  };
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Sea fight</title>
-        <meta name="description" content="Sea fight game" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Field rows={FieldProps.rows} onCellClick={handleCellClick} />
-    </div>
-  );
+const Home: NextPage = () => {
+  //const router = useRouter();
+  //router.push(`/games/`);
+  return <div> Loading</div>;
 };
 
 export default Home;
